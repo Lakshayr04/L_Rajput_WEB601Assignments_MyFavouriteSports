078cda6dd6cd24a08ff68e4c0930ee89af804812
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2024-02-03 13:49:36 -0600

    Assignment 2 Done

--- L_Rajput_MyFavouriteSports/src/app/content-card/content-card.component.ts
+++ L_Rajput_MyFavouriteSports/src/app/content-card/content-card.component.ts
@@ -1,43 +1,17 @@
-import { Component } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, Input } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content-card.component.html',
   styleUrl: './content-card.component.scss'
 })
 export class ContentCardComponent {
-  Sports:ContentList= new ContentList();
-  constructor(){
-    this.Sports.add({
-      id:1,
-      title:"Leather Ball",
-      description:"The most important raw material is leather, and the final price of a ball depends on leather quality. The highest quality of the leather is locally known as “puttha leather”.",
-      imgUrl:"https://bestcricketstore.com/wp-content/uploads/2024/01/img_5714-1.jpeg",
-      type:"Red Ball",
-      creator:"Kookaburra-colt"
-
-    })
-
-    this.Sports.add({
-      id:2,
-      title:"Cricket bat",
-      description:"The bat is wooden equipment that the batsman uses to strike the ball. It is made up of willow wood therefore it is also known as the willow.",
-      imgUrl:"https://www.cricketbestbuy.com/cdn/shop/articles/cricket-bats_600x.jpg?v=1585670436",
-      type:"Willow Bat",
-      creator:"Spartan"
-
-    })
-    this.Sports.add({ 
-      id:3,
-      title:"Gloves",
-      description:"Look for a comfortable grip, making sure to grip the bat handle firmly and reduce the chances of the bat or ball slipping out of your hand while shooting.",
-      imgUrl:"https://files.ekmcdn.com/cornishcricket/images/kookaburra-kahuna-400-batting-gloves-13968-p.jpg?v=EA3CA608-167E-45D1-BEDE-DEBAA1CD8BD3",
-      type:"Die-cutting the Glove Parts, Shelling and lining, Inserting plastic reinforcements, Constructing the web, and Lacing and Stitching",
-      creator:"Kookaburra"
-
-    })
-
-  }}
+ @Input() content:any;
+ OnClick=(id:number,name:string) =>{
+return console.log(`number ${id} and ${name} of the content`);
+  };
+}
---
+++ L_Rajput_MyFavouriteSports/src/app/content-list/content-list.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentCardComponent } from "../content-card/content-card.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-content-list',
+    standalone: true,
+    templateUrl: './content-list.component.html',
+    styleUrl: './content-list.component.scss',
+    imports: [ContentCardComponent,CommonModule]
+})
+export class ContentListComponent implements OnInit{
+  sportsContent: Content[];
+  constructor(){
+    this.sportsContent=[];
+  }
+
+  ngOnInit(): void {
+    
+  
+   this.sportsContent =[
+    {
+      id: 1,
+      title: 'Cricket Bat review',
+      description: 'The aims of this study were to experimentally measure and numerically describe the performance of cricket bats and balls. A dynamic finite element model was employed to simulate the bat-ball impact.',
+      creator: 'English Willow',
+      imgUrl: 'https://imgs.search.brave.com/_I5g0GfSgAn2048a5Z09bFO_4wFBiB1CgBzufSmlzTs/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jcmlj/a2V0ZXJzY2hvaWNl/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNi9HdW5u/LWFuZC1Nb29yZS1E/aWFtb25kLUNyaWNr/ZXQtQmF0LTIwMTkt/bWFpbi5qcGc',
+      type: 'Videos',
+      tags: ['Bat', 'Drive', 'Analysis']
+   
+     },
+    {
+      id: 2,
+      title: 'Cricket',
+      description: 'Cricket is a bat-and-ball game played between two teams of eleven players on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end, each comprising two bails balanced on three stumps.',
+      creator: 'ICC',
+      imgUrl: 'https://imgs.search.brave.com/PFLdcsBmnsXWwqdG2xdZHypVtqLxDm3sPsUFXqlgSBw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLm5k/dHZpbWcuY29tLzIw/MjQtMDIvZGk3Zzd2/bWdfaW5kaWEtYmNj/aV82MjV4MzAwXzAz/X0ZlYnJ1YXJ5XzI0/LmpwZz9pbT1GZWF0/dXJlQ3JvcCxhbGdv/cml0aG09ZG5uLHdp/ZHRoPTgwNixoZWln/aHQ9NjA1',
+      type: 'Video',
+      tags: ['Cricket', 'tutorial', 'training', 'video']
+    },
+    {
+      id: 3,
+      title: 'Ball',
+      description: 'A cricket ball is a hard, solid ball used to play cricket. A cricket ball consists of a cork core wound with string then a leather cover stitched on, and manufacture is regulated by cricket law at first-class level.',
+      creator: 'SG',
+      imgUrl: 'https://imgs.search.brave.com/dzPDmjAAdgsvRU73vjBK5VnhigYgQPxVvnP-cIbL5IA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFGRWFCUUhYc0wu/anBn',
+      type: 'Description',
+      tags: ['Ball', 'sports', 'material']
+    },
+    {
+      id: 4,
+      title: 'Cricket commentary review ',
+      description: 'A number of famous players have had a second career as writers or commentators. However, many commentators never played the game at a professional level, yet they have gone on to become famous names associated with the game.',
+      creator: 'Commentary',
+      imgUrl: 'https://imgs.search.brave.com/B1Ap-DT74_829d3gw_y9nos-d_HfJ6T3jrqjVQLO6o8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/amFncmFuam9zaC5j/b20vaW1hZ2VzLzIw/MjMvT2N0b2Jlci80/MTAyMDIzL1dvcmxk/LUN1cC1Db21tZW50/YXRvcnMud2VicA',
+      type: 'video',
+      tags: ['Commentary', 'debate', 'sports', 'pitch review']
+    },
+    {
+      id: 5,
+      title:'Football',
+      description: 'The game of association football is played in accordance with the Laws of the Game, a set of rules that has been in effect since 1863 and maintained by the IFAB since 1886.',
+      creator: 'Star Sports',
+      imgUrl: 'https://imgs.search.brave.com/k4G1vMkGTmi1HaGtOv1fLgMQDtIiVkRj49ryvwjr0OE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTA3/ODc5NDkxL3Bob3Rv/L2Zvb3RiYWxsLXBs/YXllcnMuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW9xT3ZJ/ZUpKSjczbTJubUdk/bEJVVHlvZmRJWWts/bEg0ZmNyWHJlLWFO/S3M9',
+      type: 'video',
+      tags: ['sports', 'highlights']
+     },
+    {
+      id: 6,
+      title: 'Volleyball',
+      description: 'Volleyball is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other teams court under organized rules.',
+      creator: 'TeamSports',
+      imgUrl: 'https://imgs.search.brave.com/IbTiDKLvNT9gAuuzbIw4kuPy1xOV4MSM3h5DTzYubP4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9taWth/c2FzcG9ydHNjYS0y/LmF6dXJlZWRnZS5u/ZXQvaW1nL3Byb2R1/Y3QvVjIwMFdfMS5q/cGc_ZnY9QkIxQkYw/QjQwRUVBRTYzNkE5/NTRDNjVGQzNCMEFF/QzE',
+      type: 'video',
+      tags: ['Volleyball', 'Live Cast', 'Sports']
+    }
+  ];
+
+}
+}
