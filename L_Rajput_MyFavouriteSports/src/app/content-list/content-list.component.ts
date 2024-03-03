7416b66b15c1e60e729e6f08ae880314b5ba8905
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2024-03-02 22:53:36 -0600

    Assignment Completed

--- L_Rajput_MyFavouriteSports/src/app/content-list/content-list.component.ts
+++ L_Rajput_MyFavouriteSports/src/app/content-list/content-list.component.ts
@@ -7,102 +7,110 @@ import { FormsModule } from '@angular/forms';
 import { CreateContentComponent } from '../create-content/create-content.component';
 
 @Component({
-    selector: 'app-content-list',
-    standalone: true,
-    templateUrl: './content-list.component.html',
-    styleUrl: './content-list.component.scss',
-    imports: [ContentCardComponent, CommonModule, SortTypePipe,FormsModule,CreateContentComponent]
+  selector: 'app-content-list',
+  standalone: true,
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.scss',
+  imports: [ContentCardComponent, CommonModule, SortTypePipe, FormsModule, CreateContentComponent]
 })
-export class ContentListComponent implements OnInit{
+export class ContentListComponent implements OnInit {
 
-bgColor: string = '';
-name: string='  ';
-result:string = '';
+  bgColor: string = '';
+  name: string = '  ';
+  result: string = '';
   sportsContent: Content[];
-title: any;
-  constructor(){
-    this.sportsContent=[];
+  title: any;
+  constructor() {
+    this.sportsContent = [];
   }
 
   ngOnInit(): void {
-    
-  
-   this.sportsContent =[
-    {
-      id: 1,
-      title: 'Cricket Bat review',
-      description: 'The aims of this study were to experimentally measure and numerically describe the performance of cricket bats and balls. A dynamic finite element model was employed to simulate the bat-ball impact.',
-      creator: 'English Willow',
-      type: 'Video',
-      tags: ['Bat', 'Drive', 'Analysis']
-   
-     },
-    {
-      id: 2,
-      title: 'Cricket',
-      description: 'Cricket is a bat-and-ball game played between two teams of eleven players on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end, each comprising two bails balanced on three stumps.',
-      creator: 'ICC',
-      imgUrl: 'https://imgs.search.brave.com/PFLdcsBmnsXWwqdG2xdZHypVtqLxDm3sPsUFXqlgSBw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLm5k/dHZpbWcuY29tLzIw/MjQtMDIvZGk3Zzd2/bWdfaW5kaWEtYmNj/aV82MjV4MzAwXzAz/X0ZlYnJ1YXJ5XzI0/LmpwZz9pbT1GZWF0/dXJlQ3JvcCxhbGdv/cml0aG09ZG5uLHdp/ZHRoPTgwNixoZWln/aHQ9NjA1',
-      type: 'Video',
-      tags: ['Cricket', 'tutorial', 'training', 'video']
-    },
-    {
-      id: 3,
-      title: 'Ball',
-      description: 'A cricket ball is a hard, solid ball used to play cricket. A cricket ball consists of a cork core wound with string then a leather cover stitched on, and manufacture is regulated by cricket law at first-class level.',
-      creator: 'SG',
-      imgUrl: 'https://imgs.search.brave.com/dzPDmjAAdgsvRU73vjBK5VnhigYgQPxVvnP-cIbL5IA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFGRWFCUUhYc0wu/anBn',
-      type: 'Description',
-      tags: ['Ball', 'sports', 'material']
-    },
-    {
-      id: 4,
-      title: 'Cricket commentary review ',
-      description: 'A number of famous players have had a second career as writers or commentators. However, many commentators never played the game at a professional level, yet they have gone on to become famous names associated with the game.',
-      creator: 'Commentary',
-      imgUrl: 'https://imgs.search.brave.com/B1Ap-DT74_829d3gw_y9nos-d_HfJ6T3jrqjVQLO6o8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/amFncmFuam9zaC5j/b20vaW1hZ2VzLzIw/MjMvT2N0b2Jlci80/MTAyMDIzL1dvcmxk/LUN1cC1Db21tZW50/YXRvcnMud2VicA',
-      type: 'video',
-      tags: ['Commentary', 'debate', 'sports', 'pitch review']
-    },
-    {
-      id: 5,
-      title:'Football',
-      description: 'The game of association football is played in accordance with the Laws of the Game, a set of rules that has been in effect since 1863 and maintained by the IFAB since 1886.',
-      creator: 'Star Sports',
-      imgUrl: 'https://imgs.search.brave.com/k4G1vMkGTmi1HaGtOv1fLgMQDtIiVkRj49ryvwjr0OE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTA3/ODc5NDkxL3Bob3Rv/L2Zvb3RiYWxsLXBs/YXllcnMuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW9xT3ZJ/ZUpKSjczbTJubUdk/bEJVVHlvZmRJWWts/bEg0ZmNyWHJlLWFO/S3M9',
-      type: 'video',
-      tags: ['sports', 'highlights']
-     },
-    {
-      id: 6,
-      title: 'Volleyball',
-      description: 'Volleyball is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other teams court under organized rules.',
-      creator: 'TeamSports',
-      imgUrl: 'https://imgs.search.brave.com/IbTiDKLvNT9gAuuzbIw4kuPy1xOV4MSM3h5DTzYubP4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9taWth/c2FzcG9ydHNjYS0y/LmF6dXJlZWRnZS5u/ZXQvaW1nL3Byb2R1/Y3QvVjIwMFdfMS5q/cGc_ZnY9QkIxQkYw/QjQwRUVBRTYzNkE5/NTRDNjVGQzNCMEFF/QzE',
-      type: 'description',
-      tags: ['Volleyball', 'Live Cast', 'Sports']
-    },
 
-  {
-    id: 7,
-    title: 'Badminton',
-    description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net. Although it may be played with larger teams, the most common forms of the game are singles.',
-    creator: 'TeamSports',
-    imgUrl: 'https://sportsmatik.com/uploads/matik-sports-corner/matik-know-how/badminton1_1562313127.jpg',
-    type: 'description',
-    tags: ['Badminton', 'Live Cast', 'Sports']
-  }
-];
-}
-filterResults(text: string){
-  this.sportsContent=this.sportsContent.filter(s=>s.title.toLowerCase().includes(text.toLowerCase()))
-  if(this.sportsContent.length>0){
-    this.result='Content Found';
-    this.bgColor='green' ;
 
-  }else{
-    this.result='Content Not Found';
-this.bgColor='red' ;
+    this.sportsContent = [
+      {
+        id: 1,
+        title: 'Cricket Bat review',
+        description: 'The aims of this study were to experimentally measure and numerically describe the performance of cricket bats and balls. A dynamic finite element model was employed to simulate the bat-ball impact.',
+        creator: 'English Willow',
+        type: 'Video',
+        tags: ['Bat', 'Drive', 'Analysis']
+
+      },
+      {
+        id: 2,
+        title: 'Cricket',
+        description: 'Cricket is a bat-and-ball game played between two teams of eleven players on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end, each comprising two bails balanced on three stumps.',
+        creator: 'ICC',
+        imgUrl: 'https://imgs.search.brave.com/PFLdcsBmnsXWwqdG2xdZHypVtqLxDm3sPsUFXqlgSBw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jLm5k/dHZpbWcuY29tLzIw/MjQtMDIvZGk3Zzd2/bWdfaW5kaWEtYmNj/aV82MjV4MzAwXzAz/X0ZlYnJ1YXJ5XzI0/LmpwZz9pbT1GZWF0/dXJlQ3JvcCxhbGdv/cml0aG09ZG5uLHdp/ZHRoPTgwNixoZWln/aHQ9NjA1',
+        type: 'Video',
+        tags: ['Cricket', 'tutorial', 'training', 'video']
+      },
+      {
+        id: 3,
+        title: 'Ball',
+        description: 'A cricket ball is a hard, solid ball used to play cricket. A cricket ball consists of a cork core wound with string then a leather cover stitched on, and manufacture is regulated by cricket law at first-class level.',
+        creator: 'SG',
+        imgUrl: 'https://imgs.search.brave.com/dzPDmjAAdgsvRU73vjBK5VnhigYgQPxVvnP-cIbL5IA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFGRWFCUUhYc0wu/anBn',
+        type: 'Description',
+        tags: ['Ball', 'sports', 'material']
+      },
+      {
+        id: 4,
+        title: 'Cricket commentary review ',
+        description: 'A number of famous players have had a second career as writers or commentators. However, many commentators never played the game at a professional level, yet they have gone on to become famous names associated with the game.',
+        creator: 'Commentary',
+        imgUrl: 'https://imgs.search.brave.com/B1Ap-DT74_829d3gw_y9nos-d_HfJ6T3jrqjVQLO6o8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/amFncmFuam9zaC5j/b20vaW1hZ2VzLzIw/MjMvT2N0b2Jlci80/MTAyMDIzL1dvcmxk/LUN1cC1Db21tZW50/YXRvcnMud2VicA',
+        type: 'video',
+        tags: ['Commentary', 'debate', 'sports', 'pitch review']
+      },
+      {
+        id: 5,
+        title: 'Football',
+        description: 'The game of association football is played in accordance with the Laws of the Game, a set of rules that has been in effect since 1863 and maintained by the IFAB since 1886.',
+        creator: 'Star Sports',
+        imgUrl: 'https://imgs.search.brave.com/k4G1vMkGTmi1HaGtOv1fLgMQDtIiVkRj49ryvwjr0OE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTA3/ODc5NDkxL3Bob3Rv/L2Zvb3RiYWxsLXBs/YXllcnMuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPW9xT3ZJ/ZUpKSjczbTJubUdk/bEJVVHlvZmRJWWts/bEg0ZmNyWHJlLWFO/S3M9',
+        type: 'video',
+        tags: ['sports', 'highlights']
+      },
+      {
+        id: 6,
+        title: 'Volleyball',
+        description: 'Volleyball is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other teams court under organized rules.',
+        creator: 'TeamSports',
+        imgUrl: 'https://imgs.search.brave.com/IbTiDKLvNT9gAuuzbIw4kuPy1xOV4MSM3h5DTzYubP4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9taWth/c2FzcG9ydHNjYS0y/LmF6dXJlZWRnZS5u/ZXQvaW1nL3Byb2R1/Y3QvVjIwMFdfMS5q/cGc_ZnY9QkIxQkYw/QjQwRUVBRTYzNkE5/NTRDNjVGQzNCMEFF/QzE',
+        type: 'description',
+        tags: ['Volleyball', 'Live Cast', 'Sports']
+      },
+
+      {
+        id: 7,
+        title: 'Badminton',
+        description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net. Although it may be played with larger teams, the most common forms of the game are singles.',
+        creator: 'TeamSports',
+        imgUrl: 'https://sportsmatik.com/uploads/matik-sports-corner/matik-know-how/badminton1_1562313127.jpg',
+        type: 'description',
+        tags: ['Badminton', 'Live Cast', 'Sports']
+      }
+    ];
   }
-}
+  filterResults(text: string) {
+    this.sportsContent = this.sportsContent.filter(s => s.title.toLowerCase().includes(text.toLowerCase()))
+    if (this.sportsContent.length > 0) {
+      this.result = 'Content Found';
+      this.bgColor = 'green';
+
+    } else {
+      this.result = 'Content Not Found';
+      this.bgColor = 'red';
+    }
+  }
+  addItem(item: Content) {
+    console.log("DszFd");
+    console.log(item);
+    this.sportsContent=[...this.sportsContent,item];
+    console.log("New render list");
+    console.log(this.sportsContent);
+
+  }
 }
--- L_Rajput_MyFavouriteSports/src/app/create-content/create-content.component.ts
+++ L_Rajput_MyFavouriteSports/src/app/create-content/create-content.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Content } from '../helper-files/content-interface';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-content',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './create-content.component.html',
   styleUrl: './create-content.component.scss'
 })
 export class CreateContentComponent {
+  @Output() addOutput = new EventEmitter<Content>();
   contents: Content = {
     id: 4,
     title: '',
@@ -19,10 +22,34 @@ export class CreateContentComponent {
     creator: ''
 
   }
+  errorMess: string|null = null;
   addData() {
     console.log(this.contents);
     let dataPromise = new Promise((resolve, reject) => {
-      resolve(this)
+      let testpass = false;
+
+      if (testpass) {
+        resolve("Sucees");
+      }
+      else {
+        reject("Error");
+      }
+    })
+
+    dataPromise.then((data) => {
+      this.addOutput.emit(this.contents);
+      this.contents = {
+        id: 4,
+        title: '',
+        description: '',
+        tags: [],
+        type: '',
+        creator: ''
+      }
+    })
+    dataPromise.catch((err) => {
+      this.errorMess = "Data is not vaild";
+     
     })
   }
 }
